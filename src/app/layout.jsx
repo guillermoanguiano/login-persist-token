d91e5ffce9854ca71b2df42831af009c086a669d
guillermoanguiano <jesus2001guillermo2@gmail.com>
2024-03-20 17:50:45 -0700

    consumirapi

---
+++ src/app/layout.jsx
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Dicalzeus",
+  description: "Apliacion creada con fines de aprendizaje",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
